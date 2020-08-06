b914f8e8531721ee3eee51af1e963aba0ba974a9
Andrew Dinh <git@andrewkdinh.com>
2020-08-05 19:01:14 -0700

    Finish getting processors and reviews

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from bs4 import BeautifulSoup as bs
-import re
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+from bs4 import BeautifulSoup as bs
+import re
 from typing import List
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import csv
+from datetime import datetime
+
+from pprint import pprint
 
 PATH = r'C:\Users\jsonn\Documents\Python\selenium\chromedriver.exe' #needed to put r to not get unicode error, this line is from that youtube tutorial
 
@@ -17,129 +21,51 @@ GRADING_SCALE = {4.0:'A', 3.7: 'A-',
                 0:'F'}
 GRADING_SCALE_REVERSED = {letter:num for num, letter in GRADING_SCALE.items()}
 
-def main(professor_url: str, courses_wanted: List[str], courses_not_wanted: List[str]):
-    driver = create_driver(headless=False)
-    try:
-        driver.get(professor_url)
-        # Click cookie button
-        button = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CLASS_NAME, "FullPageModal__StyledCloseButton-sc-17feuxe-1")))
-        driver.execute_script("arguments[0].click();", button)
-        #Click load more button
-        try:
-            while True:
-                button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "Buttons__BlackButton-sc-19xdot-1")))
-                driver.execute_script("arguments[0].click();", button)
-        except:
-            pass
-        professor_soup = bs(driver.page_source, "html.parser")
-    except:
-        driver.quit()
-        raise(Exception)
-    driver.quit()
+class Professor:
+    """Professor"""
+    curr_id = 0
+    csv_header = ['id', 'last_name', 'first_name', 'department', 'url']
 
-    # Scrape reviews, ratings, and difficulties
-    course_names = professor_soup.find_all("div","RatingHeader__StyledClass-sc-1dlkqw1-2 hBbYdP")
-    names = [name.string for name in course_names][::2]
-    course_reviews = professor_soup.find_all("div", "Comments__StyledComments-dzzyvm-0 dEfjGB")
-    reviews = [review.string for review in course_reviews][1:] #we don't want most helpful rating because it's repeated 
-    print("Total number of reviews:", len(reviews))
-    #print(len(names), len(reviews)) i need to figure out why reviews is half the length of names 
-    #the length of names is resolved. the source code for some reason had stuff listed twice
-    course_ratings_difficulty = professor_soup.find_all("div", re.compile("RatingValues__RatingValue-sc-6dc747-3"))
-    #ratingvalues with 747-3 are course rating and difficulty
-    ratings, difficulties = [rating.string for rating in course_ratings_difficulty[::2]], [difficulty.string for difficulty in course_ratings_difficulty[1::2]]
-    
-    # Scrape likes, dislikes, timestamps
-    likes_and_dislikes = professor_soup.find_all("div", "RatingFooter__HelpTotal-ciwspm-2 kAVFzA")
-    likes_and_dislikes = [int(like.text) for like in likes_and_dislikes][2:]
-    #print(len(likes_and_dislikes)/2)
-    like_dislike_dict = {}
-    for review in reviews:
-        like_dislike_dict[review] = likes_and_dislikes[:2]
-        like_dislike_dict[review].append(sum(likes_and_dislikes[:2]))
-        likes_and_dislikes = likes_and_dislikes[2:]   
-    #dictionary values show number of likes, dislikes, and total votes per review
-    review_times = professor_soup.find_all('div', "TimeStampStyledTimeStamp-sc-9q2r30-0 bXQmMr RatingHeaderRatingTimeStamp-sc-1dlkqw1-3 BlaCV")
-    times = [review.text for review in review_times][::2]
+    def __init__(self, last_name: str, first_name: str, department: str , url: str):
+        self.id = Professor.curr_id
+        Professor.curr_id += 1
+        self.last_name = last_name
+        self.first_name = first_name
+        self.department = department
+        self.url = url
 
-    # Scrape grades
-    divs_soup = professor_soup.find_all("div", "CourseMeta__StyledCourseMeta-x344ms-0")
-    divs = [div.text for div in divs_soup]
-    grades_list = []
+    def to_csv_row(self):
+        return [self.id, self.last_name, self.first_name, self.department, self.url]
 
-    # All the stuff
-    average_rating, average_difficulty, index, ratings_list, difficulties_list = 0, 0, 0, [], [] #by course wanted for if filter_type true
-    ratings_count = {x:0 for x in range(1,6)}
-    difficulty_count = {x:0 for x in range(1,6)}
+    def __repr__(self):
+        return str(self.last_name + ". " + self.first_name)
 
-    if courses_wanted: #because the other option was "" #some implicit booleanness
-        for name, review, rating, difficulty in zip(names, reviews, ratings, difficulties):
-            #print(name)
-            if any([name in course for course in courses_wanted]):
-                #print(review, rating, difficulty, '\n')
-                ratings_list.append(rating)
-                difficulties_list.append(difficulty)
-                if float(rating) in ratings_count:
-                    ratings_count[float(rating)] +=1
-                else:
-                    ratings_count[float(rating)] = 1
-                if float(difficulty) in difficulty_count:
-                    difficulty_count[float(difficulty)] +=1
-                else:
-                    difficulty_count[float(difficulty)] = 1
-                average_rating += float(rating)
-                average_difficulty += float(difficulty)
-                index +=1
-        for index, div in enumerate(divs):
-            if any([names[index] in course for course in courses_wanted]):
-                grade_isolator(div, grades_list)
-    else: #for filtering out courses
-        for name, review, rating, difficulty in zip(names, reviews, ratings, difficulties):
-            #print(name)
-            if not any([name in course for course in courses_not_wanted]):# i.e. class should NOT be CS61A filtered
-                #print(review, rating, difficulty, '\n')
-                ratings_list.append(rating)
-                difficulties_list.append(difficulty)
-                if float(rating) in ratings_count:# or difficulty in difficulty_count:
-                    ratings_count[float(rating)] +=1
-                else:
-                    ratings_count[float(rating)] = 1
-                if float(difficulty) in difficulty_count:
-                    difficulty_count[float(difficulty)] +=1
-                else:
-                    difficulty_count[float(difficulty)] = 1
-                average_rating += float(rating)
-                average_difficulty += float(difficulty)
-                index +=1   
-        for index, div in enumerate(divs):
-            if not any([names[index] in course for course in courses_not_wanted]):
-                grade_isolator(div, grades_list)
-    try:
-        print("Average rating under filters: {} \nAverage difficulty under filters: {}".format(average_rating/index, average_difficulty/index))
-        print("Max/min rating under filters: {}, {} \nMax/min difficulty under filters: {}, {}".format(max(ratings_list), min(ratings_list), max(difficulties_list), min(difficulties_list)))
-    except ZeroDivisionError:
-        pass
-    print("Average grade: {}".format(avg(grades_list)))
+class Review:
+    """Review"""
+    curr_ids = {}
+    csv_header = ['professor_id', 'id', 'content']
 
-    # Gender classifiers
-    gender = {0:0, 1:0 } #0 female, 1 male
-    she_series, he_series = ('she','her','hers'),("he","him","his")
-    for review in [review.lower() for review in reviews]:
-        for word in review.split():
-            if word in she_series:
-                #print(word)
-                gender[0] +=1
-            if word in he_series:
-                #print(word)
-                gender[1] +=1
-    if gender[0] == gender[1]:
-        print("gender inconclusive")
-    else:
-        reverse = {val: key for key, val in gender.items()}
-        print("gender: {}".format(reverse[max(reverse.keys())]))
+    def __init__(self, professor_id: int, content: str, sentiment: dict, attrs: dict):
+        assert(all(key in sentiment for key in ['pos', 'compound', 'neg', 'neu']))
 
-def create_driver(headless=True, implicit_wait=2):
-    """
+        if not professor_id in Review.curr_ids:
+            Review.curr_ids[professor_id] = 0
+        self.id = Review.curr_ids[professor_id]
+        Review.curr_ids[professor_id] += 1
+
+        self.professor_id = professor_id
+        self.content = content
+        self.sentiment = sentiment
+        self.attrs = attrs
+
+    def to_csv_row(self):
+        return [self.professor_id, self.id, self.content]
+
+    def __repr__(self):
+        return str(self.content)
+
+def create_driver(headless: bool=True, implicit_wait: int=2) -> webdriver:
+    """Creates and returns a Selenium driver object"""
     # Firefox
     opts = webdriver.firefox.options.Options()
     opts.headless = headless
@@ -149,6 +75,7 @@ def create_driver(headless=True, implicit_wait=2):
     opts.headless = headless
     driver = webdriver.Chrome(PATH, options=opts)
     driver.implicitly_wait(implicit_wait)
+    """
     return driver
 
 def average_grade(grades: List[str]) -> float:
@@ -184,11 +111,118 @@ def avg(grades: List[str]):
             glist.append(grade_to_number(grade))
     return average_grade(glist) if glist else 0 #if there were no grades 
 
-if __name__ == "__main__":
-    professor_url = "https://www.ratemyprofessors.com/ShowRatings.jsp?tid=1621181"
-    courses_wanted = input("Enter a comma-delimited list of specific classes to look for, otherwise leave blank: ").split(",")
-    courses_wanted = [] if courses_wanted == [''] else courses_wanted
-    courses_not_wanted = input("Enter a comma-delimited list of specific classes to avoid, otherwise leave blank: ").split(",")
-    courses_not_wanted = [] if courses_wanted == [''] else courses_wanted
+def get_professors(main_url: str ="https://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=University+of+California+Berkeley&schoolID=1072&queryoption=TEACHER&sort=alphabetical", 
+                    department_cookies: List[str] = ["Computer Science", "Electrical Engineering"]) -> List[Professor]:
+    """
+    Returns a list of professors in certain department(s)
+    
+    NOTE: Assumes UC Berkeley and EECS departments
+    """
+    # Assumes that school is UC Berkeley
+    driver = create_driver(headless=True)
+    professor_id = 0
+    professors = []
+    try:
+        # Set cookie for specific department
+        driver.get("https://www.ratemyprofessors.com/robots.txt")
+        department_cookies = ["Computer Science", "Electrical Engineering"]
+        first_run = True
+        for cookie in department_cookies:
+            driver.add_cookie({"name": "department", "value": cookie, "path": "/", })
+            driver.get(main_url)
 
-    main(professor_url, courses_wanted, courses_not_wanted)
+            if first_run:
+                # Click cookie button
+                button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "big-close")))
+                driver.execute_script("arguments[0].click();", button)
+                first_run = False
+
+            # Click load more button
+            try:
+                while True:
+                    button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".progressbtnwrap > .content")))
+                    driver.execute_script("arguments[0].click();", button)
+            except:
+                pass
+
+            # Get professor info
+            soup = bs(driver.page_source, "html.parser")
+            professors_lis = soup.find_all('li', {'id': re.compile(r'my-professor-[\d]+?')})
+            for professor_li in professors_lis:
+                url = 'https://www.ratemyprofessors.com' + professor_li.find_all('a', {'href': re.compile(r'/ShowRatings[^"]+?')})[0].attrs['href'].split("&showMyProfs")[0]
+                name = re.compile("\\d").split(professor_li.find_all('span', {'class': 'name'})[0].text, 1)[0]
+                last_name, first_name = [name.strip() for name in name.split(", ")]
+                # professors.append({'id': professor_id, 'last_name': last_name, 'first_name': first_name, 'department': cookie, 'url': url})
+                professors.append(Professor(last_name, first_name, cookie, url))
+                # professors.append([professor_id, last_name, first_name, cookie, url])
+                professor_id += 1
+    except Exception as e:
+        driver.quit()
+        raise(e)
+    driver.quit()
+    return professors
+
+def get_reviews(professor: Professor) -> List[Review]:
+    """Returns a list of reviews for a certain professor"""
+    driver = create_driver(headless=True)
+    try:
+        driver.get(professor.url)
+        # Click cookie button
+        button = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CLASS_NAME, "CCPAModal__StyledCloseButton-sc-10x9kq-2")))
+        driver.execute_script("arguments[0].click();", button)
+        #Click load more button
+        try:
+            while True:
+                button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CLASS_NAME, "Buttons__BlackButton-sc-19xdot-1")))
+                driver.execute_script("arguments[0].click();", button)
+        except:
+            pass
+        professor_soup = bs(driver.page_source, "html.parser")
+    except Exception as e:
+        driver.quit()
+        raise(e)
+    driver.quit()
+
+    analyzer = SentimentIntensityAnalyzer()
+    reviews = []
+    for rating in professor_soup.select("ul#ratingsList > li"):
+        content = rating.find_all('div', {'class': re.compile(r'Comments__StyledComments-.+-\d')})
+        if len(content) != 0: # Sometimes, they're ad div's
+            attrs = {}
+            content = content[0].text
+            rating_values = rating.find_all('div', {'class': re.compile(r'RatingValues__RatingValue-sc-.+-\d')})
+            attrs['quality'] = float(rating_values[0].text)
+            attrs['difficulty'] = float(rating_values[1].text)
+            attrs['class'] = rating.find('div', {'class': re.compile(r'RatingHeader__StyledClass-sc-.+-\d')}).text
+            likes_dislikes = rating.find_all('div', {'class': re.compile(r'RatingFooter__HelpTotal-.+-\d')})
+            attrs['likes'] = int(likes_dislikes[0].text)
+            attrs['dislikes'] = int(likes_dislikes[1].text)
+            attrs['emotion'] = rating.find('div', {'class': re.compile(r'EmotionLabel__StyledEmotionLabel-sc-.+-\d')}).text[1:]
+            date_str = re.sub(r'(st|nd|rd|th)', '', rating.find('div', {'class': re.compile(r'TimeStamp__StyledTimeStamp-sc-.+-\d')}).text)
+            attrs['date'] = datetime.strptime(date_str, '%b %d, %Y')
+            rating_tags = rating.find_all('span', {'class': re.compile(r'Tag-.+-\d')})
+            attrs['rating_tags'] = [rating_tag.text for rating_tag in rating_tags]
+
+            meta_items = rating.find_all('div', {'class': re.compile(r'MetaItem__StyledMetaItem-.+-\d')})
+            meta_items_types = {'for credit': 'for_credit', 'attendance': 'attendance', 'would take again': 'would_take_again', 'grade': 'grade', 'textbook': 'textbook'}
+            for item in meta_items:
+                for meta_item_type in meta_items_types:
+                    if meta_item_type in item.text.lower():
+                        attrs[meta_items_types[meta_item_type]] = ' '.join(item.text.split()[len(meta_item_type.split()):]).lower()
+            reviews.append(Review(professor.id, content, analyzer.polarity_scores(content), attrs))
+    return reviews
+
+def filter(reviews: List[Review], courses_wanted: List[str] = None, courses_not_wanted: List[str] = None) -> (List[Review], List[Review]):
+    """Takes in a list of reviews and returns (kept_reviews, discarded_reviews)"""
+    # TODO: FIXME
+    kept_reviews, discarded_reviews = reviews[:], []
+    return (kept_reviews, discarded_reviews)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    professors = get_professors()
+    reviews = get_reviews(professors[0])
+    # Print example review (first review for John Denero)
+    pprint([reviews[0].content, reviews[0].sentiment, reviews[0].attrs])
--- professors.py
+++ professors.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import csv
 
 def create_driver(headless=True, implicit_wait=2):
@@ -28,45 +29,60 @@ def main():
     # Assumes that school is UC Berkeley
     main_url = "https://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=University+of+California+Berkeley&schoolID=1072&queryoption=TEACHER&sort=alphabetical"
     driver = create_driver(headless=False)
+    professor_id = 0
+    professors = []
     try:
         # Set cookie for specific department
         driver.get("https://www.ratemyprofessors.com/robots.txt")
-        driver.add_cookie({"name": "department", "value": "Computer Science", "path": "/", })
+        department_cookies = ["Computer Science", "Electrical Engineering"]
+        first_run = True
+        for cookie in department_cookies:
+            driver.add_cookie({"name": "department", "value": cookie, "path": "/", })
 
-        driver.get(main_url)
+            driver.get(main_url)
 
-        # Click cookie button
-        button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "big-close")))
-        driver.execute_script("arguments[0].click();", button)
-
-        # Click load more button
-        try:
-            while True:
-                button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".progressbtnwrap > .content")))
+            if first_run:
+                # Click cookie button
+                button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, "big-close")))
                 driver.execute_script("arguments[0].click();", button)
-        except:
-            pass
+                first_run = False
 
-        # Get professor info
-        soup = bs(driver.page_source, "html.parser")
-        professors_lis = soup.find_all('li', {'id': re.compile(r'my-professor-[\d]+?')})
-        professors = []
-        for professor_li in professors_lis:
-            url = 'https://www.ratemyprofessors.com' + professor_li.find_all('a', {'href': re.compile(r'/ShowRatings[^"]+?')})[0].attrs['href'].split("&showMyProfs")[0]
-            name = re.compile("\\d").split(professor_li.find_all('span', {'class': 'name'})[0].text, 1)[0]
-            lastName, firstName = [name.strip() for name in name.split(", ")]
-            # professors.append({'last_name': lastName, 'first_name': firstName, 'url': url})
-            professors.append([lastName, firstName, url])
+            # Click load more button
+            try:
+                while True:
+                    button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".progressbtnwrap > .content")))
+                    driver.execute_script("arguments[0].click();", button)
+            except:
+                pass
+
+            # Get professor info
+            soup = bs(driver.page_source, "html.parser")
+            professors_lis = soup.find_all('li', {'id': re.compile(r'my-professor-[\d]+?')})
+            for professor_li in professors_lis:
+                url = 'https://www.ratemyprofessors.com' + professor_li.find_all('a', {'href': re.compile(r'/ShowRatings[^"]+?')})[0].attrs['href'].split("&showMyProfs")[0]
+                name = re.compile("\\d").split(professor_li.find_all('span', {'class': 'name'})[0].text, 1)[0]
+                lastName, firstName = [name.strip() for name in name.split(", ")]
+                # professors.append({'last_name': lastName, 'first_name': firstName, 'url': url})
+                professors.append([professor_id, lastName, firstName, cookie, url])
+                professor_id += 1
 
         # Write to csv
-        header = ['last_name', 'first_name', 'url']
+        header = ['id', 'last_name', 'first_name', 'department', 'url']
         with open('professors.csv', 'wt') as f:
             csv_writer = csv.writer(f)
             csv_writer.writerow(header)
             csv_writer.writerows(professors)
-    except:
+    except Exception as e:
+        print(e)
         pass
     driver.quit()
 
 if __name__ == "__main__":
     main()
+
+"""
+header = ['professor_id', 'id', 'content', 'rating', 'pos', 'neu', 'neg']
+0, 0, "I like denero", ...
+0, 1, "I hate denero", ... 
+1, 0, "I like Boser"
+"""
